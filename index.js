3b62920063762b02d1d58f2ce6251752c7ef5ffb
fazlerabby1827052 <rabby1827052@stud.kuet.ac.bd>
2025-03-03 13:01:00 +0600

    bug fix

--- index.js
+++ index.js
@@ -20,11 +20,21 @@ const addproductname= document.getElementById("addproductname");
 const addproductprice= document.getElementById("addproductprice");
 const addproductbtn= document.getElementById("addproductbtn");
 const card= document.getElementById("card");
+const addmoneysuccess= document.getElementById("addmoneysuccess");
+const spendamountcard= document.getElementById("spendamountcard");
+const totalspend= document.getElementById("totalspend");
+const lasttransactions= document.getElementById("lasttransactions");
+const transactiondiv= document.getElementById("transactiondiv");
+
+
+
 
 registersuccess.classList.add('notshow');
 loginsuccess.classList.add('notshow');
 purchaseSpan.classList.add('notshow');
 purchasesuccess.classList.add('notshow');
+
+
 
 
 function localetoOriginal(val){
@@ -43,6 +53,11 @@ function removeAClass(element,val){
 addAClass(register,'notshow');
 addAClass(login,'notshow');
 addAClass(wallet,'notshow');
+addAClass(addmoneysuccess,'notshow');
+addAClass(spendamountcard,'notshow');
+addAClass(lasttransactions,'notshow');
+addAClass(transactiondiv,'notshow');
+
 
 
 let autoincrement = 0;
@@ -57,6 +72,7 @@ function checkIslogin(){
     }
     return true;
 }
+
 function userinfo(id){
     users=userfetch();
     // console.log(users);
@@ -74,10 +90,15 @@ function userinfo(id){
 
 if(checkIslogin()===true){
     removeAClass(wallet,'notshow');
+    removeAClass(spendamountcard,'notshow');
+    removeAClass(lasttransactions,'notshow');
+    
     const val2=localStorage.getItem("currentUser");
     const val3=JSON.parse(val2);
     const cu=userinfo(val3);
-    walletspan.innerText=`${cu.walletBalance}`
+    walletspan.innerText=`${cu.walletBalance}`;
+    totalspend.innerText=`${cu.totalSpend}`;
+
 }
 else{
     removeAClass(register,'notshow');
@@ -91,7 +112,12 @@ function userfetch(){
 
 
 
-
+setTimeout(()=>{
+    if(localStorage.getItem("currentUser")!==null){
+        localStorage.removeItem("currentUser");
+        window.location.reload();
+    }
+},180000);
 
 regSubmit.addEventListener('click',(e)=>{
     e.preventDefault();
@@ -147,8 +173,9 @@ regSubmit.addEventListener('click',(e)=>{
         id:useridone,
         password:regPassword.value,
         walletBalance:0,
+        totalSpend:0,
         isLogin:false,
-        purchaseId:[],
+        transactions:[],
     }
     users.push(newUser);
     regUser.value="";
@@ -157,13 +184,15 @@ regSubmit.addEventListener('click',(e)=>{
     const v2=JSON.parse(val);
 
     localStorage.setItem("users",val);
-    registersuccess.classList.remove('notshow');
+    removeAClass(registersuccess,'notshow');
     setTimeout(()=>{
         registersuccess.classList.add('notshow');
     },2000);
-
-    addAClass(register,'notshow');
-    removeAClass(login,'notshow');
+    setTimeout(()=>{
+        addAClass(register,'notshow');
+        removeAClass(login,'notshow');
+    },2000);
+    
 });
 
 redirectLogin.addEventListener('click',(e)=>{
@@ -182,12 +211,26 @@ logSubmit.addEventListener('click',(e)=>{
         users.forEach((user)=>{
             if(user.userName===un && user.password===ps){
                 f2=true;
-                addAClass(login,'notshow');
-                removeAClass(wallet,'notshow');
-                walletspan.innerText=`${user.walletBalance}`
+                removeAClass(loginsuccess,'notshow');
+                setTimeout(()=>{
+                    addAClass(loginsuccess,'notshow');
+                },2000);
+
+                setTimeout(()=>{
+                    addAClass(login,'notshow');
+                    removeAClass(wallet,'notshow');
+                },2000);
+                
+                
+                walletspan.innerText=`${user.walletBalance}`;
+                removeAClass(spendamountcard,'notshow');
+                
+
+
                 
                 let userId1=JSON.stringify(user.id);
                 localStorage.setItem('currentUser',userId1);
+                window.location.reload();
             }
         });
         if(f2===false){
@@ -222,13 +265,19 @@ addmoneysubmit.addEventListener('click',(e)=>{
     }
     else{
         if(isNumber(addmoneyinput.value)){
-            const val=parseInt(addmoneyinput.value);
+            if(addmoneyinput.value<0){
+                alert("give a positive value please!");
+                return;
+            }
+            const val=parseFloat(addmoneyinput.value);
             const curr= localetoOriginal("currentUser");
             users=userfetch();
             const userone=userinfo(curr);
             const val2=userone.walletBalance;
             const newval=val+val2;
             userone.walletBalance=newval;
+            const transactionmessage=`Added money ${val}      | ${new Date()}`;
+            userone.transactions.push(transactionmessage);
             let nuser=[];
             const nus=users.map((user)=>{ if(user.id!==curr){
                 nuser.push(user);
@@ -238,7 +287,14 @@ addmoneysubmit.addEventListener('click',(e)=>{
             users=nuser;
             const st=JSON.stringify(users);
             localStorage.setItem("users",st);
-            window.location.reload();
+            removeAClass(addmoneysuccess,'notshow');
+            setTimeout(()=>{addAClass(addmoneysuccess,'notshow');},1000);
+
+            setTimeout(()=>{
+                window.location.reload();
+            },1000);
+            
+            
         }
         else{
             alert("please give a number");
@@ -246,9 +302,18 @@ addmoneysubmit.addEventListener('click',(e)=>{
     }
 });
 
+
 let product=[];
 
 
+function fetchproduct(){
+    if(localStorage.getItem("product")===null){
+        product=[];
+    }
+    else{
+        product = JSON.parse(localStorage.getItem("product"));
+    }
+}
 
 addproductbtn.addEventListener('click',(e)=>{card
     e.preventDefault();
@@ -257,7 +322,7 @@ addproductbtn.addEventListener('click',(e)=>{card
     const pricep=addproductprice.value;
     if(isNumber(pricep)===true){
         if(namep.length===0 || pricep.length===0){
-            alert("give product name and value");
+            alert("give product name and price");
         }
         else{
 
@@ -265,9 +330,9 @@ addproductbtn.addEventListener('click',(e)=>{card
                 name:namep,
                 price:pricep
             }
-            console.log(product);
+            fetchproduct();
+            // console.log("ama");
             product.push(obj2);
-            console.log(product);
             const str=JSON.stringify(product);
             localStorage.setItem("product",str);
             window.location.reload();
@@ -279,34 +344,47 @@ addproductbtn.addEventListener('click',(e)=>{card
     }
 });
 
+fetchproduct();
+
 if(product.length){
     let ii=0;
+
+    card.innerHTML=``;
     product.forEach((pro)=>{
         const divone=document.createElement('div');
         const val2=`pro-${ii}`;
         divone.innerHTML=`<p>${pro.name}</p>
         <p>$ ${pro.price}</p>
-        <button type="submit" id=${val2}>Add Product</button>
+        <button type="submit" id=${val2}>Buy</button>
 
         `;
+
+        card.appendChild(divone);
+
         const val3=document.getElementById(val2);
 
         val3.addEventListener('click',(e)=>{
             e.preventDefault();
             users=userfetch();
             let val4=JSON.parse(localStorage.getItem("currentUser"));
-            let useone=userinfo(val4);
-            let balanceone=useone.walletBalance;
-            if(balanceone>=pro.price){
-                let newbalance=balanceone-pro.price;
+            let userone=userinfo(val4);
+            let balanceone=userone.walletBalance;
+            const priceofproduct = parseFloat(pro.price);
+            if(balanceone>=priceofproduct){
+                let newbalance=balanceone-priceofproduct;
                 let narr=[];
                 users.forEach((user)=>{
                     if(user.id!==val4){
                         narr.push(user);
                     }
                 });
-                useone.walletBalance=newbalance;
-                narr.push(useone);
+                
+                let previousSpend=parseFloat(userone.totalSpend)+parseFloat(pro.price);
+                userone.totalSpend=previousSpend;
+                userone.walletBalance=newbalance;
+                const transactionmessage=`purchase ${pro.name} using $ ${priceofproduct}  |   ${new Date()}`;
+                userone.transactions.push(transactionmessage);
+                narr.push(userone);
                 users=narr;
                 localStorage.setItem("users",JSON.stringify(users));
                 window.location.reload();
@@ -318,8 +396,48 @@ if(product.length){
 
         });
         ii++;
-        card.innerHTML=``;
-        card.innerHTML=`${divone}`;
-        console.log(card);
+        
+        
+        // console.log(card);
     });
+}
+
+if(localStorage.getItem("currentUser")!==null){
+    const useridone = localetoOriginal("currentUser");
+    const useroneinfo = userinfo(useridone);
+    const message = useroneinfo.transactions;
+    
+    if(message.length!==0){
+        message.reverse();
+        let cntone=0;
+        let arr=[];
+        
+        message.forEach((m)=>{
+            if(cntone<5){
+                arr.push(m);
+            }
+
+            cntone++;
+        });
+        arr.reverse();
+        
+
+        // transactiondiv
+        removeAClass(transactiondiv,"notshow");
+        transactiondiv.innerText="aaaa";
+
+        transactiondiv.innerHTML=``;
+
+        arr.forEach((e)=>{
+            const cardtwo= document.createElement('div');
+            cardtwo.classList.add("givemargin");
+            cardtwo.innerText=e;
+            transactiondiv.appendChild(cardtwo);
+        });
+
+
+
+
+    }
+    
 }
